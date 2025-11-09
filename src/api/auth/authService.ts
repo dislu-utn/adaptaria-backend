04fc2fd237257e8026031855f899173d5c8c1a8b
Ciro Fernandez <cifernandez@frba.utn.edu.ar>
2025-11-09 00:34:21 -0300

    Intento compartir sesion con adaptarIA

--- src/api/auth/authService.ts
+++ src/api/auth/authService.ts
@@ -49,8 +49,18 @@ export const authService = {
         throw new PasswordChangeRequiredError();
       }
 
+      logger.trace(`[AuthService] - [login] - fetching dislu user_id`);
+      const dislu_id = await (
+        await fetch(process.env.DISLU_URL + 'api/users/get_external_id/' + foundUser.toDto().id)
+      ).json();
+
+      if (!dislu_id) logger.trace(`[AuthService] - [login] - dislu user_id not found`);
+
+      logger.trace(`[AuthService] - [login] - dislu user_id ${dislu_id}`);
+
       logger.trace(`[AuthService] - [login] - User is valid, creating session token`);
-      const token: SessionPayload = SessionPayloadSchema.parse({ id: foundUser.toDto().id });
+      const token: SessionPayload = SessionPayloadSchema.parse({ id: foundUser.toDto().id, userId: dislu_id });
+
       const access_token = jwt.sign(token, config.jwt.secret as string, { expiresIn: '12h' });
 
       let requiresSurvey: boolean | undefined;
